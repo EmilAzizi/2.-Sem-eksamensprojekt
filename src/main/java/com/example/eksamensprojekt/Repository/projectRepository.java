ec9ad20a5c27eea44dba356b88b90c5f7e73792a
zaaz <zaaz@itu.dk>
2024-04-26 13:24:49 +0200

    Started project

---
+++ src/main/java/com/example/eksamensprojekt/Repository/projectRepository.java
@@ -0,0 +1,6 @@
+package com.example.eksamensprojekt.Repository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class projectRepository {
+}
---
+++ src/main/java/com/example/eksamensprojekt/Service/projectService.java
@@ -0,0 +1,7 @@
+package com.example.eksamensprojekt.Service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class projectService {
+}
