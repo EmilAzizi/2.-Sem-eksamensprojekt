8d759fb968c303d3108328ef7091fb1625b2afbd
emila <zaaz@itu.dk>
2024-05-03 12:51:55 +0200

    Added "add user" functionality

--- src/main/java/com/example/eksamensprojekt/controller/projectController.java
+++ src/main/java/com/example/eksamensprojekt/controller/projectController.java
@@ -1,10 +1,15 @@
 package com.example.eksamensprojekt.controller;
 
+import com.example.eksamensprojekt.model.User;
 import com.example.eksamensprojekt.service.projectService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.sql.SQLException;
 
 @Controller
 @RequestMapping("projectManagement")
@@ -20,5 +25,18 @@ public class projectController {
     @GetMapping("/project")
     public String projectSelectionPage(Model model){
         return "projectSelectionPage";
+    }
+
+    @GetMapping("/createUser")
+    public String createUser(Model model){
+        User newUser = new User();
+        model.addAttribute("newUser", newUser);
+        return "createUser";
+    }
+
+    @PostMapping("/createUser")
+    public String createNewUser(@ModelAttribute User user) throws SQLException {
+        PS.createUser(user);
+        return "redirect:/projectManagement";
     }
 }
