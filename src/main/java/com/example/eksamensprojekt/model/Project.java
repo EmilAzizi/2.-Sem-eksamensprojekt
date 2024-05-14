2f5d03a37dc1a9668a43e8200896a7197fc20ad1
zaaz <zaaz@itu.dk>
2024-05-14 19:21:18 +0200

    Adding add project funktion, WIP

--- src/main/java/com/example/eksamensprojekt/model/Project.java
+++ src/main/java/com/example/eksamensprojekt/model/Project.java
@@ -1,4 +1,43 @@
 package com.example.eksamensprojekt.model;
 
 public class Project {
+    int ID = 0;
+    String name;
+    String description;
+    String date;
+
+    public Project(){
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
 }
